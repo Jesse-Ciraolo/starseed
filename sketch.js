da74ec7be91783e709c232e3b01a8209549f6482
Jesse <54598566+Jesse-Ciraolo@users.noreply.github.com>
2019-10-26 23:14:46 -0600

    Homework 9 Sketch

---
+++ sketch.js
@@ -0,0 +1,22 @@
+function setup() {
+    createCanvas(300, 400);
+  }
+  
+  function draw() {
+    background(333);
+    circle(130,70,80);
+    square(78,110,100);
+    ellipse(200,130,65,53);
+    ellipse(60,130,65,53);
+    rect(105,50,15,10);
+    rect(140,50,15,10);
+    triangle(80, 280, 130, 280, 105, 210);
+    triangle(130, 280, 180, 280, 155, 210);
+    triangle(120, 70, 135, 70, 128, 85);
+    line(110,90,150,90);
+    point(200,200);
+    textSize(32);
+      text('Jesse Ciraolo', 100, 380);
+    textSize(18);
+      text('Selft Portrait', 80, 20);
+  }
